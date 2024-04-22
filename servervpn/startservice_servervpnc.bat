b15d8b0826f48bba8e52e7bdf79dd252437c94f9
zhangergaici <1907824738@qq.com>
2024-04-22 11:25:29 +0800

    Revert "1"
    This reverts commit e6b8f88630d4ebb9e8048bfc567dcb38e6427d5f.

--- servervpn/startservice_servervpnc.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd %~dp0
-
-taskkill /F /IM servervpnc.exe
-
-start "" servervpnc.exe -c servervpnc.toml
-
-::pause
--- servervpn/startservice_servervpns.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd %~dp0
-
-taskkill /F /IM servervpns.exe
-
-start "" servervpns.exe -c servervpns.toml
-
-::pause
